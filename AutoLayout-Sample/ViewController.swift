04e8d86d5400487e782a37ac10c9b954ae8e9c17
Shinya Kumagai <roomful.rooms@gmail.com>
2020-02-28 00:15:16 +0900

    Update

--- AutoLayout-Sample/ViewController.swift
+++ AutoLayout-Sample/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var saveButton: UIButton!
+    
+    @IBOutlet private weak var cancelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let space = UILayoutGuide()
+        view.addLayoutGuide(space)
+        
+        space.widthAnchor.constraint(equalTo: saveButton.widthAnchor, multiplier: 1.0).isActive = true
+        space.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        saveButton.trailingAnchor.constraint(equalTo: space.leadingAnchor).isActive = true
+        cancelButton.leadingAnchor.constraint(equalTo: space.trailingAnchor).isActive = true
     }
-
-
 }
 
